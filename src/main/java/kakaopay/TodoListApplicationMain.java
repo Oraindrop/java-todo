e179c701556d0703e03b6aa81b819201594073ee
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2018-12-05 10:21:02 +0900

    initial commit

---
+++ src/main/java/kakaopay/TodoListApplicationMain.java
@@ -0,0 +1,11 @@
+package kakaopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplicationMain {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListApplicationMain.class);
+    }
+}
