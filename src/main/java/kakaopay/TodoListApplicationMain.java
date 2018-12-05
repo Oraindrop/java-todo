7ab946a780954c0c54c0734c404c0c6c509018b3
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2018-12-05 10:48:39 +0900

    devtools, logger setting

--- src/main/java/kakaopay/TodoListApplicationMain.java
+++ src/main/java/kakaopay/TodoListApplicationMain.java
@@ -1,11 +1,17 @@
 package kakaopay;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @SpringBootApplication
 public class TodoListApplicationMain {
+    private static final Logger logger = getLogger(TodoListApplicationMain.class);
+
     public static void main(String[] args) {
+        logger.info("hello logger");
         SpringApplication.run(TodoListApplicationMain.class);
     }
 }
