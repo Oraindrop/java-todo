f959285fea99b511176ab46acabd76e410678451
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2018-12-06 02:40:09 +0900

    putmapping init

--- src/main/java/kakaopay/todo/TodoController.java
+++ src/main/java/kakaopay/todo/TodoController.java
@@ -1,28 +1,37 @@
 package kakaopay.todo;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Controller
 @RequestMapping("/todos")
 public class TodoController {
+    private static final Logger logger = getLogger(TodoController.class);
 
     @Autowired
     private TodoRepository todoRepository;
 
     @GetMapping
-    public String showTodos(Model model){
+    public String show(Model model){
         model.addAttribute("todos", todoRepository.findAll());
         return "index";
     }
 
     @PostMapping
-    public String createTodo(Todo theTodo){
+    public String create(Todo theTodo){
         todoRepository.save(theTodo);
         return "redirect:/";
+    }
+
+    @PutMapping("/{id}")
+    public String modify(@PathVariable long id){
+        logger.info(String.valueOf(todoRepository.findById(id).orElse(null)));
+        todoRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+        return null;
     }
 }
