233643009badaa63fdd4dfc4591d8d3448b44b47
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2018-12-06 02:24:07 +0900

    insert testdata

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO TODO (id, contents, created_date, modified_date, completed) VALUES (1, '집안일', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), false);
+INSERT INTO TODO (id, contents, created_date, modified_date, completed) VALUES (2, '빨래 @1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), false);
+INSERT INTO TODO (id, contents, created_date, modified_date, completed) VALUES (3, '청소 @1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), false);
+INSERT INTO TODO (id, contents, created_date, modified_date, completed) VALUES (4, '방청소 @1 @3', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), false);
+
